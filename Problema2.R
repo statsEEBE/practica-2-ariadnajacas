3f4587c0c9dd41af8755fddbfce92b9954adb3a9
ariadna.jacas <ariadna.jacas@auleseebe.upc.edu>
2024-09-26 11:11:43 +0200

    problema 2

--- Problema2.R
+++ Problema2.R
@@ -7,3 +7,34 @@ y
 
 x <- mis_dades$Petal.Length
 x
+
+plot(x,y)
+
+xbar <- mean(x)
+xbar
+
+ybar <- mean(y)
+ybar
+
+m <- sum((x-xbar)*(y-ybar))/sum((x-xbar)^2)
+m
+b <- ybar -m*xbar
+b
+
+m*1.5+b
+
+mod <- lm(y~x)
+mod
+
+ypredicted<- predict(mod, data.frame(x=x))
+ypredicted
+
+plot(x,y)
+lines(x, ypredicted)
+
+Rsq <- sum((ypredicted-ybar)^2)/sum((y-ybar)^2)
+Rsq
+
+summary(mod)
+sqrt(Rsq)
+cor.test(x,y)
